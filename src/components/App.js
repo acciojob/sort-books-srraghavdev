f9ec6324a6602bce4e8f5ced86612980a343bc6b
Shivansh Raghav <shivanshraghav7@gmail.com>
2023-09-01 17:33:29 +0530

    sdsd

--- src/components/App.js
+++ src/components/App.js
@@ -6,19 +6,28 @@ const App = () => {
   return (
     <div>
         <h1>Books List</h1>
-        <label for='sort'>sort by</label>
+        <label for='sort'>Sort by:</label>
         <select name='sort' id='sort'>
           <option>title</option>
           <option>author</option>
           <option>publisher</option>
         </select>
-        <label for='order'>order</label>
+        <label for='order'>Order:</label>
         <select name='order' id="order">
           <option>Sorted</option>
           <option>Unsorted</option>
         </select>
         <table>
-          
+        <th>
+          <td>1</td>
+          <td>2</td>
+          <td>3</td>
+        </th>
+        <tr>
+          <td>5</td>
+          <td>6</td>
+          <td>7</td>
+        </tr>
         </table>
     </div>
   )
