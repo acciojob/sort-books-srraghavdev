d77d43be6fcbc5593a7ec6b287e630d04ca37705
Shivansh Raghav <shivanshraghav7@gmail.com>
2023-09-01 17:29:28 +0530

    sdsd

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,21 @@ import './../styles/App.css';
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+        <h1>Books List</h1>
+        <label for='sort'>sort by</label>
+        <select name='sort' id='sort'>
+          <option>title</option>
+          <option>author</option>
+          <option>publisher</option>
+        </select>
+        <label for='order'>order</label>
+        <select name='order' id="order">
+          <option>Sorted</option>
+          <option>Unsorted</option>
+        </select>
+        <table>
+          
+        </table>
     </div>
   )
 }
